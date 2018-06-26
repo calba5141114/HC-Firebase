975498560be494654efa563891f56a51d07762dc
calba5141114 <soradev4@gmail.com>
2018-06-26 14:25:23 -0700

    stripping app down

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Home extends Component{
+
+    constructor(){
+        super();
+        this.state ={
+
+        }
+    }
+
+    render(){
+
+    return(
+        <div>
+            Hello World!
+        </div>
+    );        
+
+
+    }
+
+
+}
+
+
+
+export default Home;
